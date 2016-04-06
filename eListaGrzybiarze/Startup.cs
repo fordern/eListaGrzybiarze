287cbfa638d50bee7af21e5eeb08490c73d307a2
fordern <fordern1@gmail.com>
2016-04-06 19:00:26 +0200

    Zmiana technologii

---
+++ eListaGrzybiarze/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eListaGrzybiarze.Startup))]
+namespace eListaGrzybiarze
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
